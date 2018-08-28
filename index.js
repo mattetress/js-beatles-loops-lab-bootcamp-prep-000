ec2297f7ff9137aa4237c68f1faa39d7f13e260a
Matthew Etress <mattetress@me.com>
2018-08-28 20:46:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  let array = [];
+  for (let i = 0; i < musicianArray.length; i++) {
+    array.push(`${musicianArray[i]} plays ${instrumentArray[i]}`);
+  } return array;
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+
+function johnLennonFacts(factArray) {
+  let count = 0;
+  while (count <= factArray.length) {
+    factArray[count] = `${factArray[count]}!!!`;
+    count++;
+  } return factArray;
+}
+johnLennonFacts(facts);
+
